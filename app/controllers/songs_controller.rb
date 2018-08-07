53757973f48f56c6cb2d6f833ec2c56445a63e11
Michael Pope <popemray1986@gmail.com>
2018-08-07 23:24:41 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,23 @@
 class SongsController < ApplicationController
+  before_action :set_preferences, only: [:index, :new]
+
   def index
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @songs = @artist.songs
-      end
-    else
-      @songs = Song.all
-    end
-  end
+   if params[:artist_id]
+     @artist = Artist.find_by(id: params[:artist_id])
+     if @artist.nil?
+       redirect_to artists_path, alert: "Artist not found"
+     else
+       if @preferences && @preferences.song_sort_order
+         @songs = Song.order(name: @preferences.song_sort_order)
+       else
+         @songs = @artist.songs
+       end
+     end
+   else
+     @songs = Song.all
+   end
+ end
+
 
   def show
     if params[:artist_id]
@@ -25,7 +32,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if @preferences && !@preferences.allow_create_songs
+      redirect_to songs_path
+    else
+      @song = Song.new
+    end
   end
 
   def create
@@ -66,5 +77,8 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
-end
 
+  def set_preferences
+   @preferences = Preference.first
+ end
+end
